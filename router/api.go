e61ceca06e90ed951570fef9a6a2dbaa437e05e5
尾崎仁一 <jin@jinsPC.local>
2019-01-10 11:41:32 +0900

    [add] mypageに関するAPI作成関連

--- router/api.go
+++ router/api.go
@@ -9,4 +9,29 @@ func apiRouter(api *gin.RouterGroup) {
 
 	// sampleAPI
 	api.GET("/sample/:sample_id", controller.GetSampleById)
+
+	//---マイページ画面---
+	//マイページの情報を全て取得
+	api.GET("/mypage/:user_id", controller.GetMypageFilteredById)
+
+	/*
+		//基本情報の取得
+		api.GET("/mypage/basic/:user_id", controller.GetBasicMypageFilteredById)
+
+		//フレンドマッチング情報の取得
+		api.GET("/mypage/friend/:user_id", controller.GetFriendMypageFilteredById)
+
+		//恋愛マッチング情報の取得
+		api.GET("/mypage/love/:user_id", controller.GetLoveMypageFilteredById)
+
+		//婚活マッチング情報の取得
+		api.GET("/mypage/marriage/:user_id", controller.GetMarriageMypageFilteredById)
+
+		//ルームメイトマッチング情報の取得
+		api.GET("/mypage/roommate/:user_id", controller.GetRoommateMypageFilteredById)
+
+		//---マッチング画面---
+		//選択したマッチング形式でのマッチング相手を取得
+		api.GET("/matching/:matching_id/:user_id", controller.GetMatchingFormatFilteredById)
+	*/
 }
--- service/type.go
+++ service/type.go
@@ -1,7 +1,74 @@
 package service
 
-// 自分のユーザー情報
+// sampleデータ
 type Sample struct {
 	Name    string `json:"name"`
 	Comment string `json:"comment"`
 }
+
+// マイページ情報
+type Mypage struct {
+	Basic  Basic    `json:"basic"`
+	Format []Format `json:"format"`
+}
+
+//基本的な情報
+type Basic struct {
+	FormatName string `json:"format_name"`
+	UserName   string `json:"user_name"`
+	Image1     string `json:"image1"`
+	Image2     string `json:"image2"`
+	Image3     string `json:"image3"`
+	Age        string `json:"age"`
+	Hitokoto   string `json:"hitokoto"`
+	Comment    string `json:"comment"`
+}
+
+//マッチング形式ごとのタイトルと質疑応答
+type Format struct {
+	FormatName        string              `json:"format_name"`
+	QuestionAndAnswer []QuestionAndAnswer `json:"question_and_answer"`
+}
+
+//１セットの質疑応答
+type QuestionAndAnswer struct {
+	Question string `json:"question"`
+	Answer   string `json:"answer"`
+}
+
+/*
+{
+	{
+		"qId":"1"
+		"q":"タバコは吸いますか？"
+	}
+
+	{
+		{
+			"aId":"1"
+			"qId":"1"
+			"a":"はい"
+		}
+		{
+			"aId":"2"
+			"qId":"1"
+			"a":"いいえ"
+			}
+	}
+
+
+
+
+
+
+	 {
+		"q":"職業"
+		"a":"プログラマ"
+	 }
+
+	 {
+		"q":"お酒は飲みますか？"
+		"a":"いいえ"
+	 }
+}
+*/
