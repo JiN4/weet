59b41a4a6ed54f08cf9746442dc74b1a06f6654d
kmyan009 <40896487+kmyan009@users.noreply.github.com>
2019-01-30 13:51:22 +0900

    ver3 UpdateBasics POST API

--- model/userBasics.go
+++ model/userBasics.go
@@ -1,6 +1,12 @@
 package model
 
-import "github.com/weet/service"
+import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"github.com/weet/service"
+	"io/ioutil"
+	"log"
+)
 
 // userBasicsTableにInsertする
 func CreateUserBasics(userBasics UserBasics) (UserBasics, error) {
@@ -9,6 +15,31 @@ func CreateUserBasics(userBasics UserBasics) (UserBasics, error) {
 		return UserBasics{}, err
 	}
 	return userBasics, nil
+}
+
+// userBasicsTableにUpdateする
+func UpdateUserBasics(c *gin.Context, userId uint) error {
+	body, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(string(body))
+
+	var userBasics UserBasics
+	err = json.Unmarshal(body, &userBasics)
+
+	err = db.Model(&userBasics).Where("id = ?", userId).Updates(UserBasics{
+		UserName: userBasics.UserName,
+		Image1:   userBasics.Image1,
+		Image2:   userBasics.Image2,
+		Image3:   userBasics.Image3,
+		Age:      userBasics.Age,
+		Sex:      userBasics.Sex,
+		Hitokoto: userBasics.Hitokoto,
+		Comment:  userBasics.Comment,
+	}).Error
+
+	return err
 }
 
 func GetUserBasicsById(userId uint) (service.UserBasics, error) {
