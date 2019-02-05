509789a21e1052e0d7aa47fd6498867caa4eb746
尾崎仁一 <jin@jinsPC.local>
2019-02-05 15:31:29 +0900

    [add]理想像を更新するAPIの追加

--- model/userIdealSpecials.go
+++ model/userIdealSpecials.go
@@ -1,6 +1,11 @@
 package model
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/gin-gonic/gin"
 	"github.com/weet/service"
 )
 
@@ -52,4 +57,80 @@ func GetUserIdealSpecialsById2(userId uint) (service.UserIdealSpecials2, error) 
 		userIdealSpecials = append(userIdealSpecials, userIdealSpecial)
 	}
 	return userIdealSpecials, err
+}
+
+func UpdateUserIdealSpecials(c *gin.Context, userId uint) {
+	userIdealQuestionAndAnswers := []UserIdealQuestionAndAnswer{}
+	matchingFormatId := c.PostForm("matching_format_id")
+	questionId := c.PostForm("question_id")
+	answerIds := c.PostForm("answer_ids")
+	var addAnswerIDs, deleteAnswerIDs []int
+	var intAnswerID int
+	var answerFlag bool
+
+	fmt.Println("answerIds:", answerIds)
+
+	answerIDs := strings.Split(answerIds, ",")
+
+	intMatchingFormatID, _ := strconv.Atoi(matchingFormatId)
+	intQuestionID, _ := strconv.Atoi(questionId)
+
+	//DBに登録されているanswer_idを取得
+	err := db.Select("answer_id").Where("user_id = ? and question_id = ?", userId, uint(intQuestionID)).Find(&userIdealQuestionAndAnswers).Error
+	if err != nil {
+		panic(err)
+	}
+
+	//27 28, 1 2
+	//削除対象のanswer_idを選定
+	for _, userIdealQuestionAndAnswer := range userIdealQuestionAndAnswers {
+		for _, answerID := range answerIDs {
+			intAnswerID, _ = strconv.Atoi(answerID)
+			if userIdealQuestionAndAnswer.AnswerID == uint(intAnswerID) {
+				answerFlag = true
+				break
+			}
+		}
+		if answerFlag == false {
+			deleteAnswerIDs = append(deleteAnswerIDs, int(userIdealQuestionAndAnswer.AnswerID))
+		}
+		answerFlag = false
+	}
+
+	//1 2, 27 28
+	//追加対象のanswer_idを選定
+	for _, answerID := range answerIDs {
+		for _, userIdealQuestionAndAnswer := range userIdealQuestionAndAnswers {
+			intAnswerID, _ = strconv.Atoi(answerID)
+			if uint(intAnswerID) == userIdealQuestionAndAnswer.AnswerID {
+				answerFlag = true
+				break
+			}
+		}
+		if answerFlag == false {
+			addAnswerIDs = append(addAnswerIDs, intAnswerID)
+		}
+		answerFlag = false
+	}
+
+	fmt.Println("delete:", deleteAnswerIDs)
+	fmt.Println("add:", addAnswerIDs)
+	//削除対象のanswer_idを削除
+	for _, deleteAnswerID := range deleteAnswerIDs {
+		err := db.Where("user_id = ? and answer_id = ?", userId, uint(deleteAnswerID)).Delete([]UserIdealQuestionAndAnswer{}).Error
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	//追加対象のanswer_idを削除
+	for _, addAnswerID := range addAnswerIDs {
+		CreateUserIdealQuestionAndAnswer(UserIdealQuestionAndAnswer{
+			MatchingFormatID: uint(intMatchingFormatID),
+			UserID:           userId,
+			QuestionID:       uint(intQuestionID),
+			AnswerID:         uint(addAnswerID),
+		})
+	}
+
 }
